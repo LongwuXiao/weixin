fe617c94aab78e720b9e236f84f3bda14ff5fb2d
Longwu Xiao <longwu.xiao@dcfservice.net>
2015-04-17 13:57:19 +0800

    commit weixin

---
+++ src/com/xiao/servlet/WeixinServlet.java
@@ -0,0 +1,79 @@
+package com.xiao.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.dom4j.DocumentException;
+
+import com.xiao.po.TextMessage;
+import com.xiao.util.CheckUtil;
+import com.xiao.util.MessageUtil;
+
+/**
+ * Servlet implementation class WeixinServlet
+ */
+@WebServlet("/WeixinServlet")
+public class WeixinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		String signature = request.getParameter("signature");
+		String timestamp = request.getParameter("timestamp");
+		String nonce = request.getParameter("nonce");
+		String echostr = request.getParameter("echostr");
+
+		PrintWriter out = response.getWriter();
+		if(CheckUtil.checkSignature(signature, timestamp, nonce)){
+			out.print(echostr);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		try {
+			Map<String,String> map = MessageUtil.xmlToMap(request);
+			String toUserName = map.get("ToUserName");
+			String fromUserName = map.get("FromUserName");
+			String msgType = map.get("MsgType");
+			String content = map.get("Content");
+
+			String message = null;
+			if("text".equals(msgType)){
+				TextMessage text = new TextMessage();
+				text.setFromUserName(toUserName);
+				text.setToUserName(fromUserName);
+				text.setMsgType("text");
+				text.setCreateTime(new Date().getTime());
+				text.setContent("您发送的消息是：" + content);
+				message = MessageUtil.textMessageToXml(text);
+			}
+			out.print(message);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/com/xiao/util/MessageUtil.java
@@ -0,0 +1,60 @@
+package com.xiao.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.thoughtworks.xstream.XStream;
+import com.xiao.po.TextMessage;
+
+
+public class MessageUtil {
+
+	/**
+	 * xml转为map集合
+	 * @author Longwu Xiao
+	 * @param request
+	 * @return map
+	 * @throws IOException
+	 * @throws DocumentException
+	 */
+	public static Map<String,String> xmlToMap(HttpServletRequest request) throws IOException, DocumentException{
+		Map<String,String> map = new HashMap<String,String>();
+		SAXReader reader = new SAXReader();
+
+		InputStream ins = request.getInputStream();
+		Document doc = reader.read(ins);
+
+		Element root = doc.getRootElement();
+
+		List<Element> list = root.elements();
+
+		for (Element e : list) {
+			map.put(e.getName(), e.getText());
+		}
+		ins.close();
+		return map;
+	}
+
+	/**
+	 * 将文本信息转换成xml
+	 * @param textMessage
+	 * @return
+	 */
+	public static String textMessageToXml(TextMessage textMessage){
+		XStream xstream = new XStream();
+		xstream.alias("xml", textMessage.getClass());
+		String xml = xstream.toXML(textMessage);
+		return xml;
+
+	}
+}
